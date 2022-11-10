d11769cb90a16af0392407f0f8073348a32509e8
Shahithiyan <thiyan0024@gmail.com>
2022-11-10 18:40:26 +0530

    springboot

---
+++ src/main/java/com/youtube/student/DemoClass.java
@@ -0,0 +1,16 @@
+package com.youtube.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DemoClass {
+	
+	@GetMapping("/getMessage")
+	public String getMessage() {
+		
+		return "Welcome TO My Site !";
+	}
+	
+}
